b5c84131341bba3ccdd809c1f9038354d0cf5925
Han JU @ KXEN <han.ju@kxen.com>
2013-05-07 18:17:19 +0200

    add combiner for projection to produce more compact intermediate result

---
+++ kxen-projection/src/main/java/com/kxen/han/projection/hadoop/ParallelProjectionCombiner.java
@@ -0,0 +1,25 @@
+package com.kxen.han.projection.hadoop;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import com.kxen.han.projection.hadoop.writable.TransactionTree;
+
+public class ParallelProjectionCombiner 
+extends Reducer<IntWritable, TransactionTree, IntWritable, TransactionTree> {
+	@Override
+	public void reduce(IntWritable key, Iterable<TransactionTree> values, 
+			Context context) throws IOException, InterruptedException {
+		TransactionTree tree = new TransactionTree();
+		for (TransactionTree tt : values) {
+			for (Pair<List<Long>, Long> transac : tt) {
+				tree.insertTransac(transac.getLeft(), transac.getRight().intValue());
+			}
+		}
+		context.write(key, tree.getCompressedTree());
+	}
+}
