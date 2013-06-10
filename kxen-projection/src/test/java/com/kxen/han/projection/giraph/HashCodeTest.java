f1351c6a5afd834e379551712c7b97aa1352e5de
Han JU @ KXEN <han.ju@kxen.com>
2013-06-10 16:43:27 +0200

    update hadoop to 1.1.2

---
+++ kxen-projection/src/test/java/com/kxen/han/projection/giraph/HashCodeTest.java
@@ -0,0 +1,11 @@
+package com.kxen.han.projection.giraph;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: port
+ * Date: 6/4/13
+ * Time: 3:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HashCodeTest {
+}
