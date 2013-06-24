4f5bfe6f5587bf794b66d20777705f169eb84bd7
Han JU @ KXEN <han.ju@kxen.com>
2013-06-24 11:46:17 +0200

    remove old test case that causes compilation error; TODO a new test case

--- kxen-projection/src/test/java/com/kxen/han/projection/fpg/ProjectionTest.java
+++ kxen-projection/src/test/java/com/kxen/han/projection/fpg/ProjectionTest.java
@@ -10,8 +10,5 @@ public class ProjectionTest {
 
 	@Test
 	public void testProject() throws Exception {
-		OutputLayer ol = OutputLayer.newInstance();
-		Projection.project("src/test/resources/TestExampleAutoGen", 
-				ol, 3);
 	}
 }
