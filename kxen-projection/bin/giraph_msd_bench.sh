f39a63719bbf5ab99b0496976547b29674bba425
Han JU @ KXEN <ju.han.felix@gmail.com>
2013-08-13 14:14:10 +0200

    added memory control in giraph test script

--- kxen-projection/bin/giraph_msd_bench.sh
+++ kxen-projection/bin/giraph_msd_bench.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 hadoop=/home/port/hadoop-1.1.2/bin/hadoop
+msd_file=dataset/msd/msd_train_transposed
+output_file=output/giraph/msd_transposed
 
-echo "Begin Giraph Benchmark on MSD item space ..."
-for i in 4 8 16 32 63; do
+total_mem=10240
+opts=-Xmx
+meg=m
+gig=g
+
+echo "Begin Giraph Benchmark on MSD user space ..."
+for i in 16 20 24 28 32 36 40 44 48 52 56 60 63; do
+    # calculate heap size to allocate
+    proc=`expr $i / 4`
+    mem=`expr $total_mem / $proc`
     # call the Giraph job
-    $hadoop jar ../target/kxen-projection-jar-with-dependencies.jar com.kxen.han.projection.giraph.GiraphProjection -Dmapred.child.java.opts=-Xmx1g -Ddfs.replication=1 -i dataset/msd/msd_train_data -o output/giraph/msd_full -tmp output/tmp -w $i -g 19 -s 2 -startFrom 2
+    $hadoop jar ../target/kxen-projection-jar-with-dependencies.jar com.kxen.han.projection.giraph.GiraphProjection -Dmapred.child.java.opts=$opts$mem$meg -Ddfs.replication=1 -i $msd_file -o $output_file -tmp output/tmp -w $i -g 199 -s 2 -startFrom 2
 
     if [ $? != 0 ]; then
 	echo "Error ..."
-	break;
+	# break;
     fi
 
     # after a job, delete the output folder
-    $hadoop dfs -rmr output/giraph/msd_full
+    $hadoop dfs -rmr $output_file
 done
 
 echo "Benchmark finished ..."
